5bf9cd057a5a4a4e336f644b1f1690ac78d5ca24
maxisui <55889261+maxisui@users.noreply.github.com>
2021-08-09 21:01:16 +0200

    commit

--- price_data_db.py
+++ price_data_db.py
@@ -2,15 +2,11 @@ import sqlite3
 
 import pandas as pd
 import sqlalchemy
+import yfinance as yf
 from sqlalchemy.sql.expression import table
 
 
 def load_data_from_csv():
-    # load .csv data for the AMEX exchange
-    amex_data = pd.read_csv("amex_companies.csv")
-    amex_data = amex_data[
-        ["Name", "Symbol", "Country", "IPO Year", "Sector", "Industry"]
-    ]
 
     # load .csv data for the NASDAQ exchange
     nasdaq_data = pd.read_csv("nasdaq_companies.csv")
@@ -19,6 +15,7 @@ def load_data_from_csv():
     ]
     nasdaq_data["Industry"] = nasdaq_data["Industry;"]
     nasdaq_data.drop(columns="Industry;", inplace=True)
+    nasdaq_data["Exchange"] = "nasdaq"
 
     # load .csv data for the NYSE exchange
     nyse_data = pd.read_csv("nyse_companies.csv")
@@ -27,13 +24,13 @@ def load_data_from_csv():
     ]
     nyse_data["Industry"] = nyse_data["Industry;"]
     nyse_data.drop(columns="Industry;", inplace=True)
+    nyse_data["Exchange"] = "nyse"
 
     # merge the three dataframes into one
-    exchange_data = pd.concat([amex_data, nasdaq_data, nyse_data]).reset_index(
-        drop=True
-    )
+    exchange_data = pd.concat([nasdaq_data, nyse_data])
     exchange_data["IPO_Year"] = exchange_data["IPO Year"]
     exchange_data.drop(columns="IPO Year", inplace=True)
+    # exchange_data.dropna(axis="rows", inplace=True)
     exchange_data["ID"] = exchange_data.index + 1
     return exchange_data
 
@@ -49,9 +46,18 @@ def create_connection():
 def upload_data_to_db():
     conn = create_connection()
     data = load_data_from_csv()
-    data[["Name", "Symbol", "Country", "Sector", "Industry", "IPO_Year", "ID"]].to_sql(
-        "company_info", conn, if_exists="replace", index=False
-    )
+    data[
+        [
+            "Name",
+            "Symbol",
+            "Country",
+            "Sector",
+            "Industry",
+            "IPO_Year",
+            "Exchange",
+            "ID",
+        ]
+    ].to_sql("company_info", conn, if_exists="replace", index=False)
     print("Data successfully saved to database!")
 
 
@@ -59,3 +65,19 @@ def download_data_to_df():
     conn = create_connection()
     data = pd.read_sql_table(table_name="company_info", con=conn)
     return data
+
+
+def get_pricing_data():
+
+    tickersList = download_data_to_df()["Symbol"].to_list()
+    data = []
+    failures = []
+    counter = 0
+    for ticker in tickersList:
+        data.append(yf.download(ticker).reset_index())
+        counter += 1
+        print(counter)
+    return data
+
+
+get_pricing_data()
