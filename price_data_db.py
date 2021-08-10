96a467cf211d3fe1496f45c02fd315ce911ece38
maxisui <55889261+maxisui@users.noreply.github.com>
2021-08-10 16:28:51 +0200

    commit

--- price_data_db.py
+++ price_data_db.py
@@ -1,14 +1,21 @@
 import datetime
-import sqlite3
+import os
 
-import investpy
 import pandas as pd
+import requests_cache
 import sqlalchemy
+import yfinance as yf
 from requests.api import get
 from sqlalchemy.sql.expression import table
 
-equity_database = str("equities.db")
-equities_price_database = str("equities_price.db")
+# setting some global variables
+equities_conn_string = str(os.environ["POSTGRES_DB_EQUITIES"])
+equitiies_pricedata_conn_string = str(os.environ["POSTGRES_DB_EQUITIES_PRICEDATA"])
+
+equities_engine = sqlalchemy.create_engine(equities_conn_string)
+equities_pricedata_engine = sqlalchemy.create_engine(equitiies_pricedata_conn_string)
+
+session = requests_cache.CachedSession("yfinance.cache")
 
 
 def load_data_from_csv():
@@ -40,14 +47,23 @@ def load_data_from_csv():
     return exchange_data
 
 
-def create_connection(name):
-    conn = None
-    conn = sqlalchemy.create_engine("sqlite:///" + name)
-    return conn
+def create_connection(database_name):
+    equities_conn_string = str(os.environ["POSTGRES_DB_EQUITIES"])
+    equitiies_pricedata_conn_string = str(os.environ["POSTGRES_DB_EQUITIES_PRICEDATA"])
+    if database_name == "equities":
+        equities_engine = sqlalchemy.create_engine(equities_conn_string)
+        return equities_engine
+    elif database_name == "equities_pricedata":
+        equities_pricedata_engine = sqlalchemy.create_engine(
+            equitiies_pricedata_conn_string
+        )
+        return equities_pricedata_engine
+    else:
+        print("please provide a valid database_name and try again!")
 
 
-def upload_data_to_db():
-    conn = create_connection(equity_database)
+def tosql_equities_data():
+    conn = create_connection(database_name="equities")
     data = load_data_from_csv()
     data[
         [
@@ -71,39 +87,31 @@ def download_data_to_df(database_name, table_name):
     return data
 
 
-def download_and_store_pricing_data():
-    with open("logfile.txt", "w") as log:
+def download_and_tosql_equities_pricingdata():
+    tickersList = download_data_to_df(
+        database_name="equities", table_name="company_info"
+    )["Symbol"].to_list()
 
+    conn = create_connection(database_name="equities_pricedata")
+    startdate = "1980-01-01"
+    enddate = datetime.date.today()
+    failures = []
+    counter = 0
+    for ticker in tickersList:
         try:
-            tickersList = download_data_to_df(
-                database_name=equity_database, table_name="company_info"
-            )["Symbol"].to_list()
-
-            countryList = download_data_to_df(
-                database_name=equity_database, table_name="company_info"
-            )["Country"].to_list()
-
-            conn = create_connection(equities_price_database)
-            date = "{}/{}/{}".format(
-                datetime.date.today().day,
-                datetime.date.today().month,
-                datetime.date.today().year,
+            frame = yf.Ticker(ticker, session=session).history(
+                start=startdate, end=enddate, interval="1d"
             )
+            frame.reset_index(drop=False, inplace=True)
+            frame["last_update"] = datetime.date.today()
+            frame.to_sql(ticker, conn, if_exists="replace", index=False)
 
-            counter = 0
-            for ticker in tickersList:
-                frame = investpy.get_stock_historical_data(
-                    stock=ticker, from_date="01/01/1980", to_date=date
-                ).reset_index(drop=False, inplace=True)
-
-                frame[
-                    ["Date", "Open", "High", "Low", "Close", "Volume", "Currency"]
-                ].to_sql(ticker, conn, if_exists="replace", index=False)
-                counter += 1
-                print(counter)
+            counter += 1
+            print(counter)
         except Exception as e:
-            log.write("Failed to download {0}: {1}\n".format(str(ticker), str(e)))
-            pass
+            failures.append(e)
+            failures = pd.DataFrame(data=failures)
+            failures.to_csv("failures.csv")
 
 
-download_and_store_pricing_data()
+download_and_tosql_equities_pricingdata()
